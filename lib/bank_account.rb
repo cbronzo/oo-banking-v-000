67c2da5cab5d68b7e6f668d0eeadd76658b301a9
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 16:30:25 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :balance, :status
 
 def initialize(name)
   @name = name
-  balance = 1000
-  status = "open"
+  @balance = 1000
+  @status = "open"
 end
 
 
