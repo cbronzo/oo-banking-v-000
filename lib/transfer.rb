a771311ba3e17345ec1b77dade3449571ba01b3a
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 16:48:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
 
   def execute_transaction
     
-    if sender.valid? == false
+    if !sender.valid? 
       "Transaction rejected. Please check your account balance."
     end
   end
